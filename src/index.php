e51c8578a948d9fc8dbfce2f52e6e40fa96fd0b1
Markus Thulin <macky_b@hotmail.com>
2019-01-30 22:17:51 +0100

    Added db and phpmyadmin

--- src/index.php
+++ src/index.php
@@ -1,3 +1,19 @@
 <?php
-phpinfo();
-?>
+$conn = new mysqli("db", "db_user", "my_pw", "my_db");
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id FROM my_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row['id']."<br>";
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
