674adcf0d93da2ead941f4153b6c590f75145359
Markus Thulin <macky_b@hotmail.com>
2023-01-02 10:03:22 +0100

    Add C# web project (default VS setup)

---
+++ C#Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet.Docker.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
